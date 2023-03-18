46c65f6dfb153417c1a2197a2450556b769bb819
Himanshu <kumarhimanshu2219@gmail.com>
2023-03-18 18:14:51 +0530

    dynamic routing

---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const Product = ({ params }) => {
+    console.log(params.id);
+    return (
+        <div>
+            Product
+        </div>
+    )
+}
+
+export default Product
