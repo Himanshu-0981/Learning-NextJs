926923909b3536ad211e22a0a0341fe8a142abbf
Himanshu <kumarhimanshu2219@gmail.com>
2023-03-19 13:05:26 +0530

    Router completed

---
+++ app/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+    return (
+        <>
+            <Link href="/">Home</Link>
+            <Link href="/about">Home</Link>
+        </>
+    )
+}
+
+export default Header
